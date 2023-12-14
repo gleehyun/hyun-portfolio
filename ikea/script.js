3808a1d1fecb1c31cea293c7f9c43304e8479b22
gleehyun <gleehg16@gmail.com>
2023-12-14 17:50:13 +0900

    20231214

---
+++ ikea/script.js
@@ -0,0 +1,24 @@
+// json ===========================
+const jsonFile = "./ikea.json";
+
+async function init() {
+  const response = await fetch(jsonFile);
+  const ikeaJsons = await response.json();
+  display(ikeaJsons);
+}
+function display(ikeaJsons) {
+  // sec1 slider.bestcategory=================
+  const sec1Slider = document.querySelector(".sec1_slider");
+
+  let sec1Bestitems = "";
+  ikeaJsons.bestCategory.forEach((ikeaJson) => {
+    sec1Bestitems += `
+    <div class="sec1_best_item">
+      <img src="${ikeaJson.bestImg}" alt="${ikeaJson.bestAlt}"/>
+      <p class"sec1_best_name">${ikeaJson.bestText}</p>
+    </div>
+    `;
+  });
+  sec1Slider.innerHTML = sec1Bestitems;
+}
+init();
